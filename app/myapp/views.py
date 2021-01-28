27ca7bad50ba0ed72b4dfd7f2248d43ce1094bda
logicOnAbstractions <logiconabstractions@gmail.com>
2021-01-28 13:09:50 -0500

    working static files, including basic few served by nginx

--- app/myapp/views.py
+++ app/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class IndexView(generic.View):
+    template_name = 'myapp/index.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
